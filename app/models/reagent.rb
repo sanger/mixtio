1bea4f939db9861df92ccd58bf224268099e8356
Chris Smith <cs24@sanger.ac.uk>
2015-08-11 15:38:40 +0100

    Reagent expiry_date is valid if set to today

--- app/models/reagent.rb
+++ app/models/reagent.rb
@@ -6,7 +6,7 @@ class Reagent < ActiveRecord::Base
   validate :expiry_date_cannot_be_in_the_past
 
   def expiry_date_cannot_be_in_the_past
-    if expiry_date.present? && expiry_date <= Date.today
+    if expiry_date.present? && expiry_date < Date.today
       errors.add(:expiry_date, "can't be in the past")
     end
   end
