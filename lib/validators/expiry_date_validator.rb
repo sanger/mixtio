a5de2f234cc357d542049fdf73f7d4a8542515cb
Chris Smith <cs24@sanger.ac.uk>
2015-08-17 10:26:46 +0100

    Moved expiry_date error message to locale file

--- lib/validators/expiry_date_validator.rb
+++ lib/validators/expiry_date_validator.rb
@@ -2,7 +2,7 @@ class ExpiryDateValidator < ActiveModel::EachValidator
 
   def validate_each(record, attribute, value)
     if value.present? && value < Date.today
-      record.errors.add attribute, "can't be in the past"
+      record.errors.add attribute, I18n.t('future_date')
     end
   end
 end
