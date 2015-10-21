0e0df588dbaa9ffaabaae444861e4c09c860490a
Chris Smith <cs24@sanger.ac.uk>
2015-10-21 15:11:12 +0100

    Added days to keep to consumable types

--- app/models/consumable_type.rb
+++ app/models/consumable_type.rb
@@ -1,4 +1,5 @@
 class ConsumableType < ActiveRecord::Base
 
   validates :name, presence: true, uniqueness: {case_sensitive: false}
+  validates_numericality_of :days_to_keep, greater_than: 0, if: Proc.new { |ct| ct.days_to_keep.present? }
 end
