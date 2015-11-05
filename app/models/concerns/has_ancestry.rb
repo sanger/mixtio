401abd8e74ec60f45cce1a1ab99a533c54dcaae0
Chris Smith <cs24@sanger.ac.uk>
2015-11-05 17:13:20 +0000

    Parents of ConsumableTypes can be edited

--- app/models/concerns/has_ancestry.rb
+++ app/models/concerns/has_ancestry.rb
@@ -12,6 +12,17 @@ module HasAncestry
     self
   end
 
+  def delete_parents
+    find_parents.delete_all
+    self
+  end
+
+  def set_parents(parents)
+    delete_parents
+    add_parents(parents)
+    self
+  end
+
   def add_ancestor(child, parent)
     Ancestor.create(family_name: self.class.to_s, family_id: child.id, relation_id: parent.id, relation_type: "Child")
     Ancestor.create(family_name: self.class.to_s, family_id: parent.id, relation_id: child.id, relation_type: "Parent")
@@ -36,8 +47,20 @@ module HasAncestry
   private
 
   def ancestors(relation_type)
-    ancestors = Ancestor.where(family_name: self.class.to_s, relation_type: relation_type, family_id: self.id)
+    ancestors = find_relations(relation_type)
     ancestors.empty? ? ancestors : self.class.find(ancestors.pluck(:relation_id))
+  end
+
+  def find_parents
+    find_relations("Child")
+  end
+
+  def find_children
+    find_relations("Parent")
+  end
+
+  def find_relations(relation_type)
+    Ancestor.where(family_name: self.class.to_s, relation_type: relation_type, family_id: self.id)
   end
 
 end
