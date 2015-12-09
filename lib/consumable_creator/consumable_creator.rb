8db2005c9dc71d739076494e0541f5399952f399
stevieing <stevieing@sky.com>
2015-12-09 10:50:59 +0000

    mx23 - Modified consumable creation script.

--- lib/consumable_creator/consumable_creator.rb
+++ lib/consumable_creator/consumable_creator.rb
@@ -15,17 +15,19 @@ class ConsumableCreator
 private
 
   def create_consumable_type(attributes)
-    consumable_type = ConsumableType.create(attributes.except("ingredients"))
+    consumable_type = ConsumableType.find_or_create_by(attributes.except("ingredients"))
     attributes["ingredients"].each do |k,v|
-      ingredient = ConsumableType.create(v.slice("name"))
-      consumable_type.add_parents(ingredient)
-      create_consumables(v, ingredient)
+      ingredient = ConsumableType.find_or_create_by(v.slice("name"))
+      unless consumable_type.ingredients.include?(ingredient)
+        consumable_type.add_parents(ingredient) 
+        create_consumables(v, ingredient)
+      end
     end
   end
 
   def create_consumables(attributes, ingredient)
     (0..2).each do |i|
-      Consumable.create(attributes.merge(name: ingredient.name, consumable_type_id: ingredient.id, expiry_date: Date.today+30.days, created_at: Date.today-i.months))
+      Consumable.find_or_create_by(attributes.merge(name: ingredient.name, consumable_type_id: ingredient.id, expiry_date: Date.today+30.days, created_at: Date.today-i.months))
     end
   end
 
