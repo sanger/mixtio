88d64cbe90918750fa5ef8dfc2db3d2797a9d1b7
stevieing <stevieing@sky.com>
2015-12-16 15:43:34 +0000

    Added Authentication concern. Added Sign in form.

--- app/models/users/administrator.rb
+++
@@ -1,3 +0,0 @@
-class Administrator < User
-
-end
--- config/application.rb
+++ config/application.rb
@@ -22,7 +22,7 @@ module Mixtio
 
     config.autoload_paths += %W(#{config.root}/lib/validators)
 
-    config.autoload_paths += %W(#{config.root}/app/models/users/)
+    config.filter_parameters += [:password]
 
     # Do not swallow errors in after_commit/after_rollback callbacks.
     config.active_record.raise_in_transactional_callbacks = true
