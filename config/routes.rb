fb58a7f5d91a621912649786c3060e8992771f32
stevieing <stevieing@sky.com>
2015-12-17 11:47:42 +0000

    Moved authentication into a separate module. Added a concern for controller methods. Also added a fake LDAP model to stub out LDAP calls when outside network.

--- config/routes.rb
+++ config/routes.rb
@@ -20,7 +20,7 @@ Rails.application.routes.draw do
 
   resources :sessions, only: [:new, :create, :destroy]
   get 'sign_in', to: 'sessions#new'
-  delete 'sign_out', to: 'sessions#destroy'
+  get 'sign_out', to: 'sessions#destroy'
 
   root 'batches#index'
 
--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -14,16 +14,4 @@ RSpec.describe User, type: :model do
     expect(build(:user, team: nil)).to_not be_valid
   end
 
-  it "should authenticate the user" do
-    user_1 = create(:user)
-    user_2 = build(:user)
-    allow(Ldap).to receive(:authenticate).with(user_1.username, "password").and_return(true)
-    expect(User.authenticate(user_1.username, "password")).to be_truthy
-    expect(User.authenticate(user_2.username, "password")).to be_falsey
-
-    allow(Ldap).to receive(:authenticate).with(user_1.username, "password").and_return(false)
-    expect(User.authenticate(user_1.username, "password")).to be_falsey
-    expect(User.authenticate(user_2.username, "password")).to be_falsey
-  end
-
 end
