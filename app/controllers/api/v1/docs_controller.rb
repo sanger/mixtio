7aef45b1e90e79424bb5754873e28b95877755dd
Chris Smith <cs24@sanger.ac.uk>
2016-01-14 11:59:56 +0000

    API Improvements
    Added 404s for all API resources
    Added API documentation
    Removed Consumable name from API

---
+++ app/controllers/api/v1/docs_controller.rb
@@ -0,0 +1,6 @@
+class Api::V1::DocsController < Api::V1::ApiController
+
+  def index
+    render layout: false
+  end
+end
--- spec/controllers/api/v1/consumables_controller_spec.rb
+++ spec/controllers/api/v1/consumables_controller_spec.rb
@@ -11,7 +11,6 @@ describe Api::V1::ConsumablesController, type: :request do
       consumable_response = JSON.parse(response.body, symbolize_names: true)
 
       expect(consumable_response[:id]).to eql(consumable.id)
-      expect(consumable_response[:name]).to eql(consumable.name)
       expect(consumable_response[:depleted]).to eql(consumable.depleted)
 
       batch = consumable_response[:batch]
@@ -27,6 +26,16 @@ describe Api::V1::ConsumablesController, type: :request do
       expect(consumable_type[:id]).to eql(consumable.batch.lot.consumable_type.id)
       expect(consumable_type[:name]).to eql(consumable.batch.lot.consumable_type.name)
       expect(consumable_type[:uri]).to include(api_v1_consumable_type_path consumable.batch.lot.consumable_type)
+    end
+
+    context "consumable does not exist" do
+      it "should return a 404 with an error message" do
+        get api_v1_consumable_path(:barcode => 123)
+        expect(response.status).to be(404)
+        consumable_response = JSON.parse(response.body, symbolize_names: true)
+
+        expect(consumable_response[:message]).to eq('Couldn\'t find Consumable')
+      end
     end
 
   end
