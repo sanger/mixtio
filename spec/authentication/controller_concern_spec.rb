35812e6e10a5edac1a0ce58b7d8e1e26cc5ba882
stevieing <stevieing@sky.com>
2016-02-03 08:51:25 +0000

    Removed all traces of User model and Team.

--- spec/authentication/controller_concern_spec.rb
+++ spec/authentication/controller_concern_spec.rb
@@ -2,28 +2,27 @@ require "rails_helper"
 
 RSpec.describe Authentication::ControllerConcern, type: :controller do
 
-  let!(:user)       { create(:user) }
   let(:controller)  { AnonymousController.new }
 
   before(:each) do
-    allow(Authentication::Ldap).to receive(:authenticate).with(user.username, "password").and_return(true)
-    allow(Authentication::Ldap).to receive(:authenticate).with(user.username, "badpassword").and_return(false)
+    allow(Authentication::Ldap).to receive(:authenticate).with("user1", "password").and_return(true)
+    allow(Authentication::Ldap).to receive(:authenticate).with("user1", "badpassword").and_return(false)
   end
 
   it "should be able to authenticate the user" do
-    expect(controller.authenticate?(user.username, "password")).to be_truthy
-    expect(controller.authenticate?(user.username, "badpassword")).to be_falsey
+    expect(controller.authenticate?("user1", "password")).to be_truthy
+    expect(controller.authenticate?("user1", "badpassword")).to be_falsey
   end
 
   it "should be able to check if the user is signed in" do
-    controller.authenticate?(user.username, "password")
+    controller.authenticate?("user1", "password")
     expect(controller.current_user).to be_signed_in
-    expect(controller.current_user.username).to eq(user.username)
+    expect(controller.current_user.name).to eq("user1")
     expect(controller.signed_in?).to be_truthy
   end
 
   it "should be able to sign a user out" do
-    controller.authenticate?(user.username, "password")
+    controller.authenticate?("user1", "password")
     controller.sign_out!
     expect(controller.session[:username]).to be_nil
     expect(controller.current_user).to_not be_signed_in
