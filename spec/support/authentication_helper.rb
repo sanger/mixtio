8d3f9c91e7dd683b7d82d5532df88d5aed3bba86
stevieing <stevieing@sky.com>
2016-02-03 08:51:25 +0000

    Removed all traces of User model and Team.

--- spec/support/authentication_helper.rb
+++ spec/support/authentication_helper.rb
@@ -1,9 +1,8 @@
 module AuthenticationHelper
-  def sign_in(user = nil)
-    current_user = user || create(:user)
+  def sign_in
     allow(Authentication::Ldap).to receive(:authenticate).and_return(true)
     visit sign_in_path
-    fill_in "Username", with: current_user.username 
+    fill_in "Username", with: "user1"
     fill_in "Password", with: "password"
     click_button "Sign In"
   end
