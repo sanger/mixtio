5a7d78490eddbaab1b96da23315432dee8e3dd2e
Ronan Forman <rf9@sanger.ac.uk>
2016-04-13 10:25:41 +0100

    Make kitchens case insensitive unique, add BEAGLE LIMS to initial data suppliers

--- spec/models/kitchen_spec.rb
+++ spec/models/kitchen_spec.rb
@@ -9,4 +9,10 @@ RSpec.describe Kitchen, type: :model do
     kitchen = create(:kitchen)
     expect(build(:kitchen, name: kitchen.name)).to_not be_valid
   end
+
+  it "should not allow duplicate names with differing case" do
+    kitchen = create(:kitchen)
+    expect(build(:kitchen, name: kitchen.name.upcase)).to_not be_valid
+    expect(build(:kitchen, name: kitchen.name.downcase)).to_not be_valid
+  end
 end
