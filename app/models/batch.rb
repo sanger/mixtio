8f9c40e5bff32f45285d71b961073461ef7eae14
Alex Cummins <alex.cummins@sanger.ac.uk>
2017-08-02 11:24:24 +0100

    Add getter methods use to populate Edit Batch form

--- app/models/batch.rb
+++ app/models/batch.rb
@@ -29,6 +29,28 @@ class Batch < Ingredient
     'L'
   end
 
+
+  # Getters for missing values on edit form
+  def aliquots
+    consumables.count
+  end
+
+  def aliquot_volume
+    consumables.first.volume
+  end
+
+  def aliquot_unit
+    consumables.first["unit"]
+  end
+
+  def single_barcode
+    if consumables.count == 1
+      true
+    else
+      consumables.first.barcode == consumables.second.barcode
+    end
+  end
+
   private
 
   def generate_batch_number
