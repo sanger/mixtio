32b40f64c3c8fb6ef3824b357242088f81c10708
Alex Cummins <alex.cummins@sanger.ac.uk>
2017-08-08 16:15:51 +0100

    Update schema

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170725152558) do
+ActiveRecord::Schema.define(version: 20170808142144) do
 
   create_table "audits", force: :cascade do |t|
     t.integer  "auditable_id"
