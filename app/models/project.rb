6cbe5f792edf40229831bd7c37e14c912cd5d667
Philip Jansen van Vuuren <pj5@sanger.ac.uk>
2017-10-10 09:47:23 +0100

    Remove annotate comments from project model

--- app/models/project.rb
+++ app/models/project.rb
@@ -1,13 +1,3 @@
-# == Schema Information
-#
-# Table name: projects
-#
-#  id         :integer          not null, primary key
-#  name       :string
-#  created_at :datetime         not null
-#  updated_at :datetime         not null
-#
-
 class Project < ActiveRecord::Base
   validates :name, presence: true, uniqueness: { case_sensitive: false }
 end
